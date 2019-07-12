4c24cb990bb7a11c0e4da753219133445f52466b
Kaicheng Hu <kaichenghu7@gmail.com>
2019-07-12 16:02:46 -0500

    two-ended bfs for word ladder 1

--- src/Leetcode/P0127_WordLadder.java
+++ src/Leetcode/P0127_WordLadder.java
@@ -3,6 +3,10 @@ import java.util.*;
 
 public class P0127_WordLadder {
 
+    // huahua's video helpes: https://www.youtube.com/watch?v=vWPCm69MSfs&t=822s
+
+    // approach 1: bfs
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         int min = Integer.MAX_VALUE;
         int cnt = 2;
@@ -34,6 +38,51 @@ public class P0127_WordLadder {
                 }
             }
             cnt++;
+        }
+        return 0;
+    }
+
+    // approach 2: two-ended bfs
+
+    public int ladderLength_two_end_bfs(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) return 0;
+
+        int cnt = 1;
+        Set<String> dict = new HashSet<>(wordList);
+        Set<String> set1 = new HashSet<>();
+        Set<String> set2 = new HashSet<>();
+        set1.add(beginWord);
+        set2.add(endWord);
+        while (set1.size() != 0 && set2.size() != 0) {
+            cnt++;
+            if (set2.size() < set1.size()) {
+                Set<String> temp = new HashSet<>();
+                temp = set1;
+                set1 = set2;
+                set2 = temp;
+            }
+            Set<String> temp = new HashSet<>();
+            for (String s : set1) {
+                char[] word = s.toCharArray();
+                for (int i = 0; i < word.length; i++) {
+                    char old = word[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (word[i] != c) {
+                            word[i] = c;
+                        }
+                        String newWord = new String(word);
+                        if (set2.contains(newWord)) {
+                            return cnt;
+                        }
+                        if (dict.contains(newWord)) {
+                            temp.add(newWord);
+                            dict.remove(newWord);
+                        }
+                    }
+                    word[i] = old;
+                }
+            }
+            set1 = temp;
         }
         return 0;
     }
