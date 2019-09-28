a009b4ad19efef1294ea33e3fe018fb094ab58ae
Kaicheng Hu <kaichenghu7@gmail.com>
2019-09-28 18:36:27 -0500

    review

--- src/Leetcode/P0188_StockIV.java
+++ src/Leetcode/P0188_StockIV.java
@@ -2,34 +2,90 @@ package Leetcode;
 
 public class P0188_StockIV {
 
-    // idea borrowed from discussion board
-    // Basically same as stockIII while with generalized k
+    // The idea is to use bottom-up DP. Basically same as stockIII while with generalized k
+    // The following evolution of implementation is best for me to understand
+
+    // okay, so let's first see the approach without optimization
+
+    // initial approach: (MLE)
+    // time: O(k * (n^2))
+    // space: O(k * n)
+
+    public int maxProfit_1(int k, int[] prices) {
+        if (prices == null || prices.length < 2) return 0;
+
+        int n = prices.length;
+        int[][] dp = new int[k + 1][n];
+        for (int t = 1; t <= k; t++) {
+            for (int i = 1; i < n; i++) {
+                int temp = 0;
+                for (int j = 0; j < i; j++) {
+                    temp = Math.max(temp, dp[t - 1][j] - prices[j] + prices[i]);
+                }
+                dp[t][i] = Math.max(dp[t][i - 1], temp);
+            }
+        }
+        return dp[k][n - 1];
+    }
+
+    // Now let's do some optimization for the space (TLE)
+    // time: O(k * (n^2))
+    // space: O(n)
+
+    public int maxProfit_2(int k, int[] prices) {
+        if (prices == null || prices.length < 2) return 0;
+
+        int n = prices.length;
+        int[] transaction1 = new int[n];
+        int[] transaction2 = new int[n];
+        for (int t = 0; t < k; t++) {
+            for (int i = 1; i < n; i++) {
+                int temp = 0;
+                for (int j = 0; j < i; j++) {
+                    temp = Math.max(temp, transaction1[j] - prices[j] + prices[i]);
+                }
+                transaction2[i] = Math.max(transaction2[i - 1], temp);
+            }
+            int[] transaction = transaction1;
+            transaction1 = transaction2;
+            transaction2 = transaction;
+        }
+        return transaction1[n - 1];
+    }
+
+    // Okay now TLE. We can notice that we can incorporate the innermost loop with the outer loop
+    // this is the most optimized approach under my belt right now
 
     // time: O(k * n)
     // space: O(k * n)
 
     public int maxProfit(int k, int[] prices) {
-        if (prices == null || prices.length == 0) return 0;
-
-        //if k >= n/2, then you can make maximum number of transactions.
+        if (prices == null || prices.length < 2) return 0;
         if (k >= prices.length / 2) {
-            int profit = 0;
-            for (int i = 0; i < prices.length - 1; i++) {
-                if (prices[i] < prices[i + 1]) {
-                    profit += prices[i + 1] - prices[i];
-                }
-            }
-            return profit;
+            return greedySolve(prices);
         }
         int n = prices.length;
-        int[][] dp = new int[k+1][n];
-        for (int num = 1; num < dp.length; num++) {
+        int[] transaction1 = new int[n];
+        int[] transaction2 = new int[n];
+        for (int t = 0; t < k; t++) {
             int maxDiff = -prices[0];
             for (int i = 1; i < n; i++) {
-                dp[num][i] = Math.max(dp[num][i - 1], prices[i] + maxDiff);
-                maxDiff = Math.max(maxDiff, dp[num - 1][i] - prices[i]);
+                transaction2[i] = Math.max(transaction2[i - 1], prices[i] + maxDiff);
+                maxDiff = Math.max(maxDiff, transaction1[i] - prices[i]);
+            }
+            int[] transaction = transaction1;
+            transaction1 = transaction2;
+            transaction2 = transaction;
+        }
+        return transaction1[n - 1];
+    }
+    public int greedySolve(int[] prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i + 1]) {
+                profit += prices[i + 1] - prices[i];
             }
         }
-        return dp[k][n-1];
+        return profit;
     }
 }
--- src/Leetcode/P0309_StockWithCooldown.java
+++ src/Leetcode/P0309_StockWithCooldown.java
@@ -2,55 +2,29 @@ package Leetcode;
 
 public class P0309_StockWithCooldown {
 
-    // initially couldn't figure out
-    // idea borrowed from discussion board
-    // apply state machine thinking and DP
-    // 参考state machine解法的讲解链接：
-    // https://www.youtube.com/watch?v=oL6mRyTn56M
+    // approach 1: DP
 
-    // basically, we have 3 states: buy, sell, and rest
-    // we will use 3 arrays to represent thew 3 states
-    // hold[i]: the maximum profit we can make on ith day when the last operation is buy
-    // sold[i]: the maximum profit we can make on ith day when the last operation is sold
-    // rest[i]: the maximum profit we can make on ith day when the last operation is rest
+    // hold[i]: the maximum profit if we are holding stock on ith day
+    // unhold[i]: the maximum profit if we are not holding stock on ith day
 
-    // time: O(n)
-    // space: O(n)
+    // hold[i] comes from maximum between:
+    //  1. buy on ith day: unhold[i - 2] - prices[i]
+    //  2. no transaction on ith day: hold[i - 1]
+    // unhold[i] comes from maximum between:
+    //  1. sell on ith day: hold[i - 1] + prices[i]
+    //  2. no selling on ith day: unhold[i - 1]
 
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0)   return 0;
-        int[] hold = new int[prices.length + 1];
-        int[] sold = new int[prices.length + 1];
-        int[] rest = new int[prices.length + 1];
+        if (prices == null || prices.length < 2) return 0;
+
+        int n = prices.length;
+        int[] hold = new int[n];
+        int[] unhold = new int[n];
         hold[0] = -prices[0];
-
-        for (int i = 1; i <= prices.length; i++) {
-            // hold[i] can represent buy stock on ith day or have bought the
-            // stock before i and not buy on this day (where hold[i - 1] means),
-            // but still, the last operation is buy for hold[i]
-            hold[i] = Math.max(rest[i-1] - prices[i-1], hold[i-1]);
-            // we can only sell when the last operation is buying
-            sold[i] = hold[i-1] + prices[i-1];
-            rest[i] = Math.max(rest[i-1], sold[i-1]);
+        for (int i = 1; i < n; i++) {
+            hold[i] = Math.max(hold[i - 1], -prices[i] + (i > 1 ? unhold[i - 2] : 0));
+            unhold[i] = Math.max(unhold[i - 1], hold[i - 1] + prices[i]);
         }
-        return Math.max(sold[prices.length], rest[prices.length]);
-    }
-
-    // space optimize: since state only relies on their previous states, we can use 3 state
-    // variables to keep track of all the states
-
-    // time: O(n)
-    // space: O(1)
-    public int maxProfit_space_optimize(int[] prices) {
-        if (prices == null || prices.length == 0)   return 0;
-        int sold = 0, rest = 0, hold = -prices[0];
-
-        for (int i = 0; i < prices.length; i++) {
-            int prevRest = rest;
-            rest = Math.max(sold, rest);
-            sold = hold + prices[i];
-            hold = Math.max(hold, prevRest - prices[i]);
-        }
-        return Math.max(rest, sold);
+        return unhold[n - 1];
     }
 }
